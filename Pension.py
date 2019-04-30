c9270ee794aaa10456576d6b07b7161dbfdfa03d
Mohd Jalaluddin Ahmad <9w2tja@gmail.com>
2019-05-01 07:48:03 +0800

    Add files via upload

--- Pension.py
+++ Pension.py
@@ -1,55 +1,65 @@
-yrs =0
 def main():
-    InputAge = int(input("Enter your age: "))
-    InputNumberOfMonths = int(input("Enter number of months of service: "))
-    InputFirstSalaries = float(input("Enter first of three highest salaries: "))
-    InputSecondSalaries = float(input("Enter second of three highest salaries: "))
-    InputThirdSalaries = float(input("Enter third of three highest salaries: "))
+    inputAge = int(input("Enter your age: "))
+    inputMonths = int(input("Enter number of months of service: "))
 
-    yrs = round(NumberOfMonths(InputNumberOfMonths),0)
+    #print(retiredAge(inputAge))
+    #print(monthService(inputMonths))
+    #print(canRetied(inputAge, inputMonths))
+    
+    if canRetied(inputAge, inputMonths) == True:
+        inputFirstSalaries = float(input("Enter first of three highest salaries: "))
+        inputSecondSalaries = float(input("Enter second of three highest salaries: "))
+        inputThirdSalaries = float(input("Enter third of three highest salaries: "))
 
-    if canRetire(InputAge, yrs) == True:
-        ave = round(AverageAnnualSalary(InputFirstSalaries, InputSecondSalaries, InputThirdSalaries),2)
-        p = round(minPerRate(yrs),2)
-        amountPension = round(pension(p, ave),2)
-        output(amountPension)
+        #print(averageSalary(inputFirstSalaries, inputSecondSalaries, inputThirdSalaries))
+        ave = averageSalary(inputFirstSalaries, inputSecondSalaries, inputThirdSalaries)
+
+        #print(monthService(inputMonths))
+        yrs = monthService(inputMonths)
+
+        #print(perRate(yrs))
+        p = perRate(yrs)
+
+        #print(pension(p, ave))
+        amountPension = pension(p, ave)
+        print("Annual pension: ${0:0,.2f}".format(amountPension))
+
     else:
         print("--- Can Retire at age 55 with at least 20 years of service ---")
         main()
-        
-def canRetire(Age, Month):
-    if Age >= 55 and Month >= 20:
+
+def retiredAge(age):
+    if age >= 55:
         return True
     else:
         return False
 
-def AverageAnnualSalary(firstS, secondS, thirdS):
-    return (firstS + secondS + thirdS)/3
+def monthService(month):
+    return month/12
 
-def NumberOfMonths(months):
-    return months/12
-
-def percentageRate(year):
-    if year <= 5:
-        rate = 0.015 * year
-    elif year <= 10:
-        rate = (0.015 * 5) + (0.0175 * (year-5))
+def canRetied(age, month):
+    if retiredAge(age) == True:
+        if monthService(month) >= 20:
+            return True
+        else:
+            return False
     else:
-        rate = (0.015 * 5) + (0.0175 * 5) + (0.02 * (year-10))
-    return rate
+        return False
+        
+def averageSalary(first, second, third):
+    return (first + second + third)/3
 
-def minPerRate(yearRate):
-    minRate = round(percentageRate(yearRate),2)
-    if minRate <= .8:
-        return minRate
+def perRate(year):
+    rate = ((1.5 * 5) + (1.75 * 5) + (2.0 * (year - 10))) /100
+    return maxRate(rate)
+
+def maxRate(rate):
+    if rate <=.8:
+        return rate
     else:
         return .8
 
 def pension(p, ave):
     return p * ave
-    
-def output(amountPension):
-    print("Annual pension: ${0:0,.2f}".format(amountPension))
 
 main()
-
